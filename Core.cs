909ae2bb5f2e904f8b1455149b2d4f913d17962f
igromanru <igromanru@yahoo.de>
2024-09-29 17:36:33 +0200

    Init

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(HPAchievementsEnabler.Core), "HPAchievementsEnabler", "1.0.0", "igromanru", null)]
+[assembly: MelonGame("Eek", "House Party")]
+
+namespace HPAchievementsEnabler
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
